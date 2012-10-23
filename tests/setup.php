3e2a713c88072fefc2fa29227aaaa41179e4921f
Michael De Wildt <michael.dewildt@99designs.com>
2012-10-23 23:10:43 +1100

    Updated test setup to suit new API

--- tests/setup.php
+++ tests/setup.php
@@ -9,71 +9,55 @@
 // Require the bootstrap
 require_once('bootstrap.php');
 
-echo 'Running Dropbox Test Suite Setup...' . PHP_EOL;
-
 // Check if a token is already stored
-if(file_exists('oauth.token')){
-	$token = file_get_contents('oauth.token');
-	if(unserialize($token) !== false){
-		// Prompt for use input
-		while(empty($response)){
-			echo 'Do you wish to delete the stored access token? (y/n): ';
-			$response = strtolower(trim(fgets(STDIN)));
-		}
-		// Exit if user chooses not to delete the token
-		if($response != 'y' && $response != 'yes'){
-			exit('Quitting setup. Your access token was not deleted.');
-		} else {
-			unlink('oauth.token');
-			echo 'Access token deleted. Continuing setup.' . PHP_EOL;
-		}
-	}
-}
+if(!file_exists('oauth.token')) {
+    echo 'Running Dropbox Test Suite Setup...' . PHP_EOL;
 
-while(empty($consumerKey)){
-	echo 'Please enter your consumer key: ';
-	$consumerKey = trim(fgets(STDIN));
-}
+    while(empty($consumerKey)){
+        echo 'Please enter your consumer key: ';
+        $consumerKey = trim(fgets(STDIN));
+    }
 
-while(empty($consumerSecret)){
-	echo 'Please enter your consumer secret: ';
-	$consumerSecret = trim(fgets(STDIN));
-}
+    while(empty($consumerSecret)){
+        echo 'Please enter your consumer secret: ';
+        $consumerSecret = trim(fgets(STDIN));
+    }
 
-try {
-	// Set up the OAuth consumer
-	$OAuth = new OAuth_Consumer_Curl($consumerKey, $consumerSecret);
+    try {
+        // Set up the OAuth consumer
+        $OAuth = new OAuth_Consumer_Curl($consumerKey, $consumerSecret);
 
-	$token = $OAuth->getRequestToken();
+        $token = $OAuth->getRequestToken();
 
-	$OAuth->setToken($token);
+        $OAuth->setToken($token);
 
-	// Generate the authorisation URL and prompt user
-	echo "Generating Authorisation URL...\r\n\r\n";
-	echo "===== Begin Authorisation URL =====\r\n";
-	echo $OAuth->getAuthoriseUrl() . PHP_EOL;
-	echo "===== End Authorisation URL =====\r\n\r\n";
-	echo "Visit the URL above and allow the SDK to connect to your account\r\n";
-	echo "Press any key once you have completed this step...";
-	fgets(STDIN);
+        // Generate the authorisation URL and prompt user
+        echo "Generating Authorisation URL...\r\n\r\n";
+        echo "===== Begin Authorisation URL =====\r\n";
+        echo $OAuth->getAuthoriseUrl() . PHP_EOL;
+        echo "===== End Authorisation URL =====\r\n\r\n";
+        echo "Visit the URL above and allow the SDK to connect to your account\r\n";
+        echo "Press any key once you have completed this step...";
+        fgets(STDIN);
 
-	// Acquire the access token
-	echo "Acquiring access token...\r\n";
+        // Acquire the access token
+        echo "Acquiring access token...\r\n";
 
-	$OAuth->getAccessToken();
-	$token = serialize(array(
-		'token' => $token,
-		'consumerKey' => $consumerKey,
-		'consumerSecret' => $consumerSecret,
-	));
+        $accessToken = $OAuth->getAccessToken();
+        $token = serialize(array(
+            'token' => $accessToken,
+            'consumerKey' => $consumerKey,
+            'consumerSecret' => $consumerSecret,
+        ));
 
-	// Write the access token to disk
-	if(@file_put_contents('oauth.token', $token) === false){
-		throw new \Dropbox\Exception('Unable to write token to file');
-	} else {
-		exit('Setup complete! You can now run the test suite.');
-	}
-} catch(\Dropbox\Exception $e) {
-	echo $e->getMessage() . PHP_EOL;
-	exit('Setup failed! Please try running setup again.');
+        // Write the access token to disk
+        if(@file_put_contents('oauth.token', $token) === false){
+            throw new \Dropbox\Exception('Unable to write token to file');
+        } else {
+            echo 'Setup complete! Running the test suite.';
+        }
+    } catch(\Dropbox\Exception $e) {
+        echo $e->getMessage() . PHP_EOL;
+        exit('Setup failed! Please try running setup again.');
+    }
 }
